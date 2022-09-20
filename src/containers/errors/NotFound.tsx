9620c2fc133c44bf9c1b1441acb2f728b3848245
ansariymahdi <ansariymahdi@gmail.com>
2022-09-20 12:20:38 +0430

    change error page

--- src/containers/errors/NotFound.tsx
+++ src/containers/errors/NotFound.tsx
@@ -1,7 +1,9 @@
 import {Error} from "@components/errorPage/Error";
 
 const NotFound = () => {
-  return <Error code={404} msg="صفحه مورد نظر در ریحان گلد موجود نیست." />;
+  return (
+    <Error code={404} msg="صفحه مورد نظر در طلا و جواهر ریحان   موجود نیست." />
+  );
 };
 
 export default NotFound;
